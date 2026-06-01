86401e7b290a7d8a319c65529250498606bd6c9b
yurmar <yurmarchuk@gmail.com>
2026-06-01 10:46:18 +0800

    Add import news

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -20,18 +23,25 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDefaultSort(['publishedAt' => 'DESC'])
+            ->setPaginatorPageSize(20);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('title', 'Заголовок'),
-            AssociationField::new('category', 'Категория'),
             AssociationField::new('source', 'Источник'),
+            AssociationField::new('category', 'Категория')->hideOnIndex(),
             TextareaField::new('summary', 'Краткое описание')->hideOnIndex(),
-            UrlField::new('externalUrl', 'Ссылка'),
+            UrlField::new('externalUrl', 'Ссылка')->hideOnIndex(),
             UrlField::new('imageUrl', 'Изображение')->hideOnIndex(),
-            DateTimeField::new('publishedAt', 'Опубликовано'),
-            DateTimeField::new('createdAt', 'Добавлено')->hideOnForm(),
+            DateTimeField::new('publishedAt', 'Дата публикации'),
+            DateTimeField::new('createdAt', 'Дата получения')->hideOnForm(),
         ];
     }
 }
